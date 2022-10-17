9263e89ef8e8563e4516d823aacf893ea895e04c
AnassAoulad <anass.aoulad.bh@gmail.com>
2022-10-17 17:57:04 +0200

    [Fix] entrypoint.sh change option for chrome flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lhci autorun --chrome-flags="google-chrome-stable --headless" --collect.url="$1" --collect.numberOfRuns="1" --upload.target="filesystem" --upload.outputDir="./report-lhci" --upload.ignoreDuplicateBuildFailure"true" --no-lighthouserc="true"
+lhci autorun --chrome-flags="--headless" --collect.url="$1" --collect.numberOfRuns="1" --upload.target="filesystem" --upload.outputDir="./report-lhci" --upload.ignoreDuplicateBuildFailure"true" --no-lighthouserc="true"
 wait $!
 
 result=$(cat report-lhci/manifest.json | jq '.[0] .summary' | tr -d {}'"')
